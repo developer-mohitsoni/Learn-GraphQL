2598eaeffd6eb2fc0fc953967674ffc5a59d57df
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-22 15:51:06 +0530

    Basic try to fetching Query in GraphQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import apolloServer from "./config/apolloServer.js";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
 
 const app: Application = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   return res.json({
@@ -11,6 +18,14 @@ app.get("/", (req: Request, res: Response) => {
     message: "App is working",
   });
 });
+
+const startApolloServer = async () => {
+  await apolloServer.start();
+
+  app.use("/graphql", expressMiddleware(apolloServer));
+};
+
+startApolloServer();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
