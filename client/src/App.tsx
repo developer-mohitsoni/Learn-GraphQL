c54c31247b4465ae7254b8d63b7ea14046d97616
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-22 17:07:18 +0530

    Refactor App component to include a form for adding todos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const App = () => {
+  const [todo, setTodo] = useState("");
+
+  const handleSubmitted = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // TODO: Send the todo to your GraphQL API
+
+    console.log("The todo is: " + todo);
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-red-500 text-3xl font-bold">Hello I am Mohit Soni</h1>
+    <div className="h-screen flex justify-center items-center bg-blue-50">
+      <div className="w-full px-2 md:w-[600px] rounded-md bg-blue-100 h-[60vh]">
+        <h1 className="text-xl font-bold my-2">
+          What's you are doing today ?{" "}
+        </h1>
+        <form onSubmit={handleSubmitted}>
+          <input
+            className="w-full h-10 p-2 outline-none rounded-md"
+            type="text"
+            placeholder="Enter your todo here..."
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+        </form>
+      </div>
     </div>
   );
 };
