b9347927982cc4c6b10781ea5fed761657c2e58d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-22 17:12:32 +0530

    Refactor main.tsx to use Apollo Client for GraphQL integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,19 @@
 import { createRoot } from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
