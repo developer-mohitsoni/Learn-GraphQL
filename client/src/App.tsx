a6dea4a44d5c90204676053b2eba78bd762b569d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-22 16:56:21 +0530

    Refactor App component to display a centered heading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-red-500 text-3xl font-bold">Hello I am Mohit Soni</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
