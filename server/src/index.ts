79ebd2f3dcaf03a3cff459c83f4e3ecb502c8191
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-26 10:51:52 +0530

    Refactor import statements and format code for consistency

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
-import express, { Application, Request, Response } from "express";
+import express, {
+	type Application,
+	type Request,
+	type Response,
+} from "express";
 import "dotenv/config";
-import apolloServer from "./config/apolloServer.js";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import cors from "cors";
+import apolloServer from "./config/apolloServer.js";
 
 const app: Application = express();
 
@@ -13,20 +17,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({
-    status: 200,
-    message: "App is working",
-  });
+	res.json({
+		status: 200,
+		message: "App is working",
+	});
 });
 
 const { url } = await startStandaloneServer(apolloServer, {
-  listen: {
-    port: 4000,
-  },
+	listen: {
+		port: 4000,
+	},
 });
 
 console.log(`Apollo Server started at ${url}`);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
